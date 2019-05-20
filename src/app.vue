47b1003092cde466ab0232d30ea45ff8faf70131
huhongtao <huhongtao@jd.com>
2019-05-20 09:47:28 +0800

    vues

---
+++ src/app.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="viewport">
+        vue
+    </div>
+</template>
+
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import vue from 'vue';
+import app from 'app.vue';
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+});
